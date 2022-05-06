9ec62ee581c62731bcbc88c6ac9827f058f6fafa
Kumar Aashish Raj <94250210+k-araj@users.noreply.github.com>
2022-05-07 02:17:19 +0530

    Add files via upload

---
+++ splash.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:robocore/main.dart';
+//import 'package:clay_containers/clay_containers.dart';
+
+void main(){
+  runApp( const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'CORSIT Login',
+    home:Splash(),
+  ));
+}
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 1),
+            ()=>Navigator.pushReplacement(context,
+            MaterialPageRoute(builder:
+                (context) =>
+                const MyHomePage()
+            )
+        )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: const Center(
+        child: CircleAvatar(
+          radius: 100.0,
+          backgroundImage: AssetImage('images/3.jpg'),
+        ),
+      ),
+    );
+  }
+}
